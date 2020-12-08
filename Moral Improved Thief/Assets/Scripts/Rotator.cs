f9cfa30827502e4421986d9f02030dc421698ac8
moyamong <daniel123800@gmail.com>
2020-12-08 21:31:36 +0900

    MapTemplate8, storyteller making
    Former-commit-id: 4cb253f847b33bade49ee733904380f888bb5e4e

---
+++ Moral Improved Thief/Assets/Scripts/Rotator.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rotator : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+    public float angularSpeed;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        this.transform.Rotate(new Vector3(0f, 0f, angularSpeed * Time.deltaTime));
+    }
+}
---
+++ Moral Improved Thief/Assets/Scripts/StoryManager.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using System.IO;
+
+public class StoryManager : MonoBehaviour
+{
+    public string path;
+    public Text textUI;
+    // Start is called before the first frame update
+    void Start()
+    {
+        string currentPath = ".\\Assets\\Story Texts\\";
+        currentPath += path;
+        ReadText(currentPath);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void ReadText(string path)
+    {
+        string text = File.ReadAllText(path);
+        string[] texts = text.Split(new char[] { '\t' });
+        foreach (string paragraph in texts)
+        {
+            Debug.Log(paragraph);
+        }
+    }
+}
