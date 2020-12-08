454eca651689a0f8da2ae6ebd6f646e42b5b09e0
moyamong <daniel123800@gmail.com>
2020-12-08 17:32:43 +0900

    Yamong's Elevator
    Former-commit-id: 3ec681f6aba50fbac00ed4caeb95194558109559

---
+++ Moral Improved Thief/Assets/Scripts/Elevator.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Elevator : MonoBehaviour
+{
+    Transform startPos;
+    Transform endPos;
+    Transform elevator;
+    public float period;
+    //bool goingUp;
+    public AnimationCurve ac;
+    float time;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        startPos = transform.Find("StartPos");
+        endPos = transform.Find("EndPos");
+        elevator = transform.Find("Elevator");
+        elevator.position = endPos.position;
+        time = 0f;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        elevator.position = Vector3.Lerp(startPos.position, endPos.position, ac.Evaluate(time / period));
+        time += Time.deltaTime;
+        if (time > period) time -= period;
+
+    }
+}
