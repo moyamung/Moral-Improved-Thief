856c40ef4f11406b1748d0efa2dcc40c9cac5b33
moyamong <daniel123800@gmail.com>
2020-12-09 10:28:52 +0900

    Lobby
    Former-commit-id: efed0c1cd4e10318d9a0fe8a82e20c38611300f8

---
+++ Moral Improved Thief/Assets/Scripts/LobbyPortal.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LobbyPortal : MonoBehaviour
+{
+    public int number;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void UsePortal(GameObject Player)
+    {
+        GameManager gm = FindObjectOfType<GameManager>();
+        if (number == 4)
+        {
+            if (gm.stage[1] == GameManager.GameState.Open && gm.stage[2] == GameManager.GameState.Open && gm.stage[3] == GameManager.GameState.Open)
+            {
+                SceneManager.LoadScene("LastMap");
+            }
+        }
+        else 
+        {
+            if (gm.stage[number] == GameManager.GameState.Unseen)
+            {
+                SceneManager.LoadScene("First");
+            }
+        }
+    }
+}
